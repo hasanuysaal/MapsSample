71c06e76e8b3c3733df1a7cc55a7df4fca0ad9e2
hasanuysaal <hsnuysaal@icloud.com>
2022-09-02 15:34:17 +0300

    get user's location

--- MapsSample/ViewController.swift
+++ MapsSample/ViewController.swift
@@ -8,14 +8,29 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, MKMapViewDelegate{
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         mapView.delegate = self
+        locationManager.delegate = self
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization() // add cause of accessing in "info.plist" file
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print(locations[0].coordinate.latitude)
+        print(locations[0].coordinate.longitude)
+        
     }
 
 
